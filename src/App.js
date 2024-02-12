e811e6627bef19e94fc0423ca2c0f7a813199bfe
SamGreat <samantha@greatorex.org.uk>
2024-02-12 13:17:19 +0000

    Task 4.6 Enabling Chat facilities

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import uitoolkit from "@zoom/videosdk-ui-toolkit";
 import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
 import { generateToken } from "./Functions";
 
-const features = ["video", "audio"];
+const features = ["video", "audio", "chat"];
 
 function App() {
   const onStartSession = (sessionSettings) => {
