2f47ea049d134a639e1f5fb5d04b34f7427fabc6
SamGreat <samantha@greatorex.org.uk>
2024-02-12 13:13:12 +0000

    Task 4.1 Importing the depencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import SettingsForm from "./Form";
+import uitoolkit from "@zoom/videosdk-ui-toolkit";
+import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
 
 function App() {
   const onStartSession = (sessionSettings) => {};
