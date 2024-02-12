28deee366f14ddb019af03e3c56822667c7120f7
SamGreat <samantha@greatorex.org.uk>
2024-02-12 13:22:02 +0000

    Task 5.1 Adding the session joined event

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
 import SettingsForm from "./Form";
+import "./App.css";
 import uitoolkit from "@zoom/videosdk-ui-toolkit";
 import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
 import { generateToken } from "./Functions";
@@ -15,8 +15,17 @@ function App() {
     configureSdk(sessionSettings, 0);
   };
 
+  const onSessionJoined = () => {
+    var sMessage = document.getElementById("sessionMessage");
+    sMessage.innerText = "You are in a session!!";
+  };
+
+  const onSessionEnded = () => {
+    var sMessage = document.getElementById("sessionMessage");
+    sMessage.innerText = "";
+  };
+
   const configureSdk = async (settings, role) => {
-    console.log("Configuring session", settings);
     const jwtToken = await generateToken(settings.sessionName, role, settings.sessionId, settings.sessionPasscode);
 
     const config = {
@@ -29,11 +38,15 @@ function App() {
 
     var sessionContainer = document.getElementById("sessionContainer");
     uitoolkit.joinSession(sessionContainer, config);
+
+    uitoolkit.onSessionJoined(onSessionJoined);
+    uitoolkit.onSessionClosed(onSessionEnded);
   };
 
   return (
     <div className="App">
       <SettingsForm OnStartSession={onStartSession} OnJoinSession={OnJoinSession} />
+      <div id="sessionMessage" />
       <div id="sessionContainer"></div>
     </div>
   );
